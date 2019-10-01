1d95dd454b67b34efbfdd0462a542cc669f5a0af
Ellen Buelow <buelowek1225@gmail.com>
2019-10-01 11:36:07 -0500

    Font Awesome, images, navbar design

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// https://travishorn.com/integrating-multiple-stylesheets-in-a-single-site-e720256edcee
+
+// check this out with Will tonight
+
+// 'use strict';
+
+// var gulp = require('gulp');
+// var concatCss = require('gulp-concat-css');
+
+// gulp.task('css', function() {
+//   return gulp.src('css/*.css')
+//     .pipe(concatCss('style.css'))
+//     .pipe(gulp.dest('.'));
+// });
